d8a7eeb4db45eac5cb50437bf8a3257a2158f8a5
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-04-21 23:29:56 +0100

    Starting And Stopping Electron

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,3 +1,8 @@
 // import electron from 'electron'
-
 const electron = require("electron");
+const { app } = electron; // represent the overall running process of electron on our machine
+
+// event-based programming
+app.on("ready", () => {
+    console.log("app is now ready");
+});
